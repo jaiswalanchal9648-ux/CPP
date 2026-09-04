9620caa118cc458c0f5730e4b11a0912143af0fe
Anchal Jaiswal <jaiswalanchal9648@gmail.com>
2026-09-04 20:39:47 +0530

    Set STL

---
+++ Set/multiset.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+#include<set>
+ int main(){
+   multiset<int>s;
+
+   s.insert(1);
+   s.insert(2);
+   s.insert(3);
+   s.insert(4);
+   s.insert(5);
+
+
+   s.insert(1);
+   s.insert(2);
+   s.insert(3);
+
+   cout<<"size= "<<s.size()<<endl;
+   for(int val : s){              // ascending order data
+    cout<<val<<" ";
+   }
+   cout<<endl;
+
+ }
---
+++ Set/unorderedset.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+#include<unordered_set>
+ int main(){
+   unordered_set<int>s;
+
+   s.insert(6);
+   s.insert(2);
+   s.insert(3);
+   s.insert(4);
+   s.insert(5);
+
+
+   s.insert(1);
+   s.insert(2);
+   s.insert(3);
+
+   cout<<"size= "<<s.size()<<endl;
+   for(int val : s){              // random order data
+    cout<<val<<" ";
+   }
+   cout<<endl;
+
+ }
